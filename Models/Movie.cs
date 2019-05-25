672f197c986cf62fd50f2ec4267fdd0bc9daefbf
Pavel Kochkin <kach93@mail.ru>
2019-05-25 09:23:10 +0300

    added rating property

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id{ get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; } = new DateTime(1993,07,13);
+
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+    }
+}
